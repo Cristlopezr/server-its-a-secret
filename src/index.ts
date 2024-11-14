d649154adfe5bec6ecbea081a2bfbc48600885a4
cristian <cristianlopezr.931@gmail.com>
2024-11-14 15:11:06 -0300

    Change outdir folder

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const MAX_PLAYERS = 4;
 
 const io = new Server(Number(process.env.PORT) ?? 3001, {
     cors: {
-        origin: ['http://localhost:3000'],
+        origin: [process.env.APP_URL as string],
     },
 });
 
